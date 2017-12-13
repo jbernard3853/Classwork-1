fcd1fabda6d5779fdeb9376f5932f4952729d577
BT_1N3_16 <BT_1N3_16@BT_1N3_16.bktech.local>
2017-12-13 13:15:39 -0500

    lab 4_1

---
+++ Classwork/src/Lab4_1/Card.java
@@ -0,0 +1,63 @@
+package Lab4_1;
+
+/**
+ * Lab 4_1
+ * @author Raymond Cheung
+ */
+
+public class Card {
+	private String rank;
+	private String suit;
+	private int pointValue;
+	
+	/**
+	 * Creates a card
+	 * @param rank
+	 * @param suit
+	 * @param pointValue
+	 */
+	public Card(String rank, String suit, int pointValue) {
+		this.rank = rank;
+		this.suit = suit;
+		this.pointValue = pointValue;
+	}
+	
+	/**
+	 * Checks if a card is equal in pointValue to another card
+	 * @param otherCard card to be compared to
+	 * @return
+	 */
+	public boolean equals(Card otherCard) {
+		if (this.getPointValue() == otherCard.getPointValue()) {
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public String toString() {
+		return "Card [rank=" + rank + ", suit=" + suit + ", pointValue=" + pointValue + "]";
+	}
+	
+	public String getRank() {
+		return rank;
+	}
+
+	public void setRank(String rank) {
+		this.rank = rank;
+	}
+
+	public String getSuit() {
+		return suit;
+	}
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+	public int getPointValue() {
+		return pointValue;
+	}
+	public void setPointValue(int pointValue) {
+		this.pointValue = pointValue;
+	}
+	
+}
---
+++ Classwork/src/Lab4_1/Deck.java
@@ -0,0 +1,12 @@
+package Lab4_1;
+
+import java.util.ArrayList;
+
+public class Deck {
+	private ArrayList <Card> unDealt;
+	private ArrayList <Card> Dealt;
+	
+	public Deck(String[] ranks, String[] suits, int[] pointValues) {
+		ArrayList<Card> deck = new addAll()
+	}
+}
